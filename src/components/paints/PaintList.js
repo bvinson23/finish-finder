228c92de508b55271b1ad58c48df3c6e6b3b4b16
bvinson23 <brandonvinson23@gmail.com>
2021-04-23 14:39:29 -0500

    updated the list to have use effect

--- src/components/paints/PaintList.js
+++ src/components/paints/PaintList.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getAllPaints, getPaintById } from "../../modules/PaintManager";
 
 export const PaintList = () => {
+    const getPaints = () => {
+        return getAllPaints().then(paintsfromAPI => {
+            console.log(paintsfromAPI)
+        });
+    };
+
+    useEffect(() => {
+        getPaints();
+    }, []);
+
     return (
         <div className="container-cards">
-            We'll put some paint here soon...
+            We'll put some paints here soon...
         </div>
     )
 }
