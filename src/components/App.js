741ca4398db5d4089c44f9e90fb47ea8c4393c35
bvinson23 <brandonvinson23@gmail.com>
2021-04-23 11:23:43 -0500

    setup for the app

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ApplicationViews } from "./ApplicationViews";
+import "./Finish-Finder.css";
+
+export const FinishFinder = () => {
+  return (
+    <>
+      <ApplicationViews />
+    </>
+  )
+}
