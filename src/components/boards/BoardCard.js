f4c0a4300de5db3ed0d33796f954d069d081e5eb
bvinson23 <brandonvinson23@gmail.com>
2021-04-28 14:28:30 -0500

    styling for boards complete

--- src/components/boards/BoardCard.js
+++ src/components/boards/BoardCard.js
@@ -1,41 +1,50 @@
 import React from "react";
 import "./Board.css";
+import { FaPen, FaTrashAlt } from "react-icons/fa";
 
 export const BoardCard = ({ board }) => {
     return (
         <>
             <div className="board">
-                <h2>Board Title</h2>
-                <section className="board-section">
-                    <article className="board-carpet">
-                        <img src={board.carpet.image} alt={board.carpet.name}></img>
-                        <div className="carpet-details">
-                            <h3>{board.carpet.name}</h3>
-                            <p></p>
-                            <p>{board.carpet.brand}</p>
-                            <p>${board.carpet.price}</p>
-                            <p>{board.carpet.styleId}</p>
-                        </div>
-                    </article>
-                </section>
-                <section className="board-section">
-                    <article className="board-paint">
-                        <img src={board.paint.image} alt={board.paint.name}></img>
-                        <div className="paint-details">
-                            <h3>{board.paint.name}</h3>
-                            <p>{board.paint.brand}</p>
-                        </div>
-                    </article>
-                </section>
-                <section className="board-section">
-                    <article className="board-base">
-                        <img src={board.base.image} alt={board.base.name}></img>
-                        <div className="base-details">
-                            <h3>{board.base.name}</h3>
-                            <p>{board.base.brand}</p>
-                        </div>
-                    </article>
-                </section>
+                <div className="board-title">
+                    <div className="board-title__components">
+                        <h2>{board.name}</h2>
+                        <p><FaPen /><FaTrashAlt /></p>
+                    </div>
+                </div>
+                <div className="board-content">
+                    <section className="board-section">
+                        <article className="board-carpet">
+                            <img src={board.carpet.image} alt={board.carpet.name}></img>
+                            <div className="carpet-details">
+                                <h4>{board.carpet.name}</h4>
+                                <p>Color: {board.carpet.colorName}</p>
+                                <p>Manufacturer: {board.carpet.brand}</p>
+                                <p>Price: ${board.carpet.price}</p>
+                                {/* <p>{board.carpet.styleId}</p> */}
+                            </div>
+                        </article>
+                    </section>
+                    <section className="board-section">
+                        <article className="board-paint">
+                            <img src={board.paint.image} alt={board.paint.name}></img>
+                            <div className="paint-details">
+                                <h4>{board.paint.name}</h4>
+                                <p>Manufacturer: {board.paint.brand}</p>
+                            </div>
+                        </article>
+                    </section>
+                    <section className="board-section">
+                        <article className="board-base">
+                            <img src={board.base.image} alt={board.base.name}></img>
+                            <div className="base-details">
+                                <h4>{board.base.name}</h4>
+                                <p>Color: {board.base.colorName}</p>
+                                <p>Manufacturer: {board.base.brand}</p>
+                            </div>
+                        </article>
+                    </section>
+                </div>
             </div>
         </>
     )
