41fe163369fbfbec358d40fcfe3e55c561afa386
bvinson23 <brandonvinson23@gmail.com>
2021-05-04 15:44:11 -0500

    still trying to get edit to work

--- src/components/boards/EditSelectionPreviewCard.js
+++ src/components/boards/EditSelectionPreviewCard.js
@@ -17,13 +17,7 @@ export const EditSelectionPreviewCard = ({ paint, base, carpet }) => {
     const { boardId } = useParams();
 
     const handleFieldChange = evt => {
-        let editedBoard = {
-            paintId: board.paintId,
-            carpetId: board.carpetId,
-            baseId: board.baseId,
-            name: board.name,
-            userId: parseInt(sessionStorage.getItem("app_user_id"))
-        }
+        const editedBoard = { ...board }
         let selectedVal = evt.target.value
         if (evt.target.id.includes("Id")) {
             selectedVal = parseInt(selectedVal)
@@ -48,75 +42,163 @@ export const EditSelectionPreviewCard = ({ paint, base, carpet }) => {
     useEffect(() => {
         getBoardById(boardId)
             .then(board => {
-                console.log(board)
                 setBoard(board);
             })
     }, [boardId])
 
+    // useEffect(() => {
+    //     getBoardById(boardId)
+    //         .then(board => {
+    //             setBoard(board)
+    //         })
+    // }, [paint])
+
+    // useEffect(() => {
+    //     getBoardById(boardId)
+    //         .then(board => {
+    //             setBoard(board)
+    //         })
+    // }, [base])
+
+    // useEffect(() => {
+    //     getBoardById(boardId)
+    //         .then(board => {
+    //             setBoard(board)
+    //         })
+    // }, [carpet])
+    console.log(carpet)
     return (
-        <form className="boardForm">
-            <aside className="selections-container">
-                <fieldset>
-                    <div className="form-group">
-                        <label htmlFor="name">Title:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            onChange={handleFieldChange}
-                            required autoFocus
-                            className="form-control"
-                            placeholder="Name your board"
-                            value={board.name} />
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <div className="selection__paint">
-                        <select
-                            id="paint"
-                            className="hide-me"
-                            onInput={handleFieldChange}
-                            value={board.paintId}>
-                            <option value="paint.id"></option>
-                        </select>
-                        <h4>Paint</h4>
-                        <p>{board.paint?.name}</p>
-                        <div className="selection__image">
-                            <img src={board.paint?.image} alt={board.paint?.name}></img>
-                        </div>
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <div className="selection__base">
-                        <select
-                            id="base"
-                            className="hide-me"
-                            onChange={handleFieldChange}
-                            value={board.baseId} />
-                        <h4>Vinyl Base</h4>
-                        <p>{board.base?.name}</p>
-                        <div className="selection__image">
-                            <img src={board.base?.image} alt={board.base?.name}></img>
-                        </div>
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <div className="selection__carpet">
-                        <select
-                            id="carpet"
-                            className="hide-me"
-                            onChange={handleFieldChange}
-                            value={board.carpetId} />
-                        <h4>Carpet</h4>
-                        <p>{board.carpet?.name}</p>
-                        <div className="selection__image">
-                            <img src={board.carpet?.image} alt={board.carpet?.name}></img>
-                        </div>
-                    </div>
-                </fieldset>
-                <Link to={"/"}>
-                    <button type="button" className="button-save" onClick={updateExistingBoard}> -Save Board- </button>
-                </Link>
-            </aside>
-        </form>
+        <>
+            {carpet.length <= 0 ?
+                <form className="boardForm">
+                    <aside className="selections-container">
+                        <fieldset>
+                            <div className="form-group">
+                                <label htmlFor="name">Title:</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    onChange={handleFieldChange}
+                                    required autoFocus
+                                    className="form-control"
+                                    placeholder="Name your board"
+                                    value={board.name} />
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="selection__paint">
+                                <select
+                                    id="paint"
+                                    className="hide-me"
+                                    onInput={handleFieldChange}
+                                    value={board.paintId}>
+                                    <option value="paint.id"></option>
+                                </select>
+                                <h4>Paint</h4>
+                                <p>{board.paint?.name}</p>
+                                <div className="selection__image">
+                                    <img src={board.paint?.image} alt={board.paint?.name}></img>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="selection__base">
+                                <select
+                                    id="base"
+                                    className="hide-me"
+                                    onChange={handleFieldChange}
+                                    value={board.baseId} />
+                                <h4>Vinyl Base</h4>
+                                <p>{board.base?.name}</p>
+                                <div className="selection__image">
+                                    <img src={board.base?.image} alt={board.base?.name}></img>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="selection__carpet">
+                                <select
+                                    id="carpet"
+                                    className="hide-me"
+                                    onChange={handleFieldChange}
+                                    value={board.carpetId} />
+                                <h4>Carpet</h4>
+                                <p>{board.carpet?.name}</p>
+                                <div className="selection__image">
+                                    <img src={board.carpet?.image} alt={board.carpet?.name}></img>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <Link to={"/"}>
+                            <button type="button" className="button-save" onClick={updateExistingBoard}> -Save Board- </button>
+                        </Link>
+                    </aside>
+                </form> :
+                <form className="boardForm">
+                    <aside className="selections-container">
+                        <fieldset>
+                            <div className="form-group">
+                                <label htmlFor="name">Title:</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    onChange={handleFieldChange}
+                                    required autoFocus
+                                    className="form-control"
+                                    placeholder="Name your board"
+                                    value={board.name} />
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="selection__paint">
+                                <select
+                                    id="paint"
+                                    className="hide-me"
+                                    onInput={handleFieldChange}
+                                    value={board.paintId}>
+                                    <option value="paint.id"></option>
+                                </select>
+                                <h4>Paint</h4>
+                                <p>{paint.name}</p>
+                                <div className="selection__image">
+                                    <img src={paint.image} alt={paint.name}></img>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="selection__base">
+                                <select
+                                    id="base"
+                                    className="hide-me"
+                                    onChange={handleFieldChange}
+                                    value={board.baseId} />
+                                <h4>Vinyl Base</h4>
+                                <p>{base.name}</p>
+                                <div className="selection__image">
+                                    <img src={base.image} alt={base.name}></img>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <fieldset>
+                            <div className="selection__carpet">
+                                <select
+                                    id="carpet"
+                                    className="hide-me"
+                                    onChange={handleFieldChange}
+                                    value={board.carpetId} />
+                                <h4>Carpet</h4>
+                                <p>{carpet.name}</p>
+                                <div className="selection__image">
+                                    <img src={carpet.image} alt={carpet.name}></img>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <Link to={"/"}>
+                            <button type="button" className="button-save" onClick={updateExistingBoard}> -Save Board- </button>
+                        </Link>
+                    </aside>
+                </form>
+            }
+        </>
     )
 }
